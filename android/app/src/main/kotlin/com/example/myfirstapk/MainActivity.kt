14ecf8a0fb22512e8b982981ff4154d07355da9b
esrael23 <93195740+esrael23@users.noreply.github.com>
2021-12-24 06:40:09 -0800

    my 1st app

---
+++ android/app/src/main/kotlin/com/example/myfirstapk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myfirstapk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp( MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+        title: Text('Esrael Ermias'),
+      centerTitle: true,
+
+      ),
+    body: Center(
+      child: Text('my first up'),
+    ),
+
+
+
+    ),
+  ));
+
