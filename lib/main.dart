1729bb05c1acbb734b8254357063a8e934b5ff68
esrael23 <93195740+esrael23@users.noreply.github.com>
2021-12-24 06:56:03 -0800

    section

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@ import 'package:flutter/material.dart';
 void main() => runApp( MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-        title: Text('Esrael Ermias'),
+        title: Text('Esrael Ermias '),
       centerTitle: true,
 
       ),
     body: Center(
-      child: Text('my first up'),
+      child: Text('my first app section 2 4th year'),
     ),
 
 
